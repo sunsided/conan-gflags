27f04080746e51f85f5911559b9b975c0bb5d7ab
Markus Mayer <widemeadows@gmail.com>
2016-09-11 16:50:12 +0200

    fixed debug build to use Windows Debug runtimes

--- build.py
+++ build.py
@@ -4,5 +4,17 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="sunside", channel="snapshot")
     builder.add_common_builds(shared_option_name="gtest:shared", pure_c=False)
+
+    patched_builds = []
+    for settings, options in builder.builds:
+
+        # Selecting the debug runtimes (MTd, MDd) on Windows for Debug builds
+        if settings["compiler"] == "Visual Studio" and settings["build_type"] == "Debug":
+            if not settings["compiler.runtime"].endswith("d"):
+                settings["compiler.runtime"] = settings["compiler.runtime"] + "d"
+        patched_builds.append([settings, options])
+
+    builder.builds = patched_builds
+
     builder.run()
 
